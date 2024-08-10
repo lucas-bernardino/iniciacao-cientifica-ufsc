cac4ff8918e25fd2f40cf8b85cfaa14b006ce754
lucas-bernardino <lucasgber@gmail.com>
2024-08-10 09:28:48 -0300

    Add socket with props and refactor Sensor.tsx to work with sockets data

--- frontend/vite-project/components/Sensor.tsx
+++ frontend/vite-project/components/Sensor.tsx
@@ -1,27 +1,24 @@
 import "./Sensor.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function Sensor() {
-  // dar o fetch na API que recebeu os dados da rasp.
+interface ISocket {
+  socket: any
+}
 
-  const [sensorData, setSensorData] = useState([]);
+function Sensor({ socket }: ISocket) {
+  const [sensorData, setSensorData] = useState<any>([]);
 
-  async function getSensorData() {
-    const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/receber_ultimo", { headers: { 'ngrok-skip-browser-warning': 'true' } });
-    const data = await response.json();
-    setSensorData(data);
-  }
+  let url: string = import.meta.env.VITE_BACKEND_URL;
+  url = url.replace("http", "ws"); // Change this to HTTPS if not in localhost
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      getSensorData();
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []); // https://iq.js.org/questions/react/how-to-update-a-component-every-second
-
-  // SE NAO RODAR DA PRIMEIRA VEZ, PRECISA RODAR O SERVIDOR, RODAR O PYTHON REQUEST, COMENTAR TODO ESSE RETURN, CARREGAR A PAGINA, DEPOIS DESCOMENTA O RETURN E VAI ESTAR TUDO OK
-  // ARRUMAR ESSE BUG
+    socket.on("send", (response: string) => {
+      setSensorData(response);
+    });
+    return () => {
+      socket.off("send");
+    }
+  }, [])
 
   return (
     <div className="sensor-container">
@@ -30,30 +27,30 @@ function Sensor() {
         <div className="sensor-data-title">
           Velocidade Angular X
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["vel_x"]).toFixed(2)}
+            {sensorData && Number(sensorData["vel_x"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Velocidade Angular Y
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["vel_y"]).toFixed(2)}
+            {sensorData && Number(sensorData["vel_y"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Velocidade Angular Z
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["vel_z"]).toFixed(2)}
+            {sensorData && Number(sensorData["vel_z"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Módulo
           <div className="sensor-data-value">
-            {sensorData[0] &&
+            {sensorData &&
               Number(
                 Math.sqrt(
-                  sensorData[0]["vel_x"] ** 2 +
-                  sensorData[0]["vel_y"] ** 2 +
-                  sensorData[0]["vel_z"] ** 2,
+                  sensorData["vel_x"] ** 2 +
+                  sensorData["vel_y"] ** 2 +
+                  sensorData["vel_z"] ** 2,
                 ),
               ).toFixed(2)}
           </div>
@@ -65,30 +62,30 @@ function Sensor() {
         <div className="sensor-data-title">
           Aceleração X
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["acel_x"]).toFixed(2)}
+            {sensorData && Number(sensorData["acel_x"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Aceleração Y
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["acel_y"]).toFixed(2)}
+            {sensorData && Number(sensorData["acel_y"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Aceleração Z
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["acel_z"]).toFixed(2)}
+            {sensorData && Number(sensorData["acel_z"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Módulo
           <div className="sensor-data-value">
-            {sensorData[0] &&
+            {sensorData &&
               Number(
                 Math.sqrt(
-                  sensorData[0]["acel_x"] ** 2 +
-                  sensorData[0]["acel_y"] ** 2 +
-                  sensorData[0]["acel_z"] ** 2,
+                  sensorData["acel_x"] ** 2 +
+                  sensorData["acel_y"] ** 2 +
+                  sensorData["acel_z"] ** 2,
                 ),
               ).toFixed(2)}
           </div>
@@ -100,30 +97,30 @@ function Sensor() {
         <div className="sensor-data-title">
           Roll
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["roll"]).toFixed(2)}
+            {sensorData && Number(sensorData["roll"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Pitch
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["pitch"]).toFixed(2)}
+            {sensorData && Number(sensorData["pitch"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Yaw
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["yaw"]).toFixed(2)}
+            {sensorData && Number(sensorData["yaw"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Módulo
           <div className="sensor-data-value">
-            {sensorData[0] &&
+            {sensorData &&
               Number(
                 Math.sqrt(
-                  sensorData[0]["roll"] ** 2 +
-                  sensorData[0]["pitch"] ** 2 +
-                  sensorData[0]["yaw"] ** 2,
+                  sensorData["roll"] ** 2 +
+                  sensorData["pitch"] ** 2 +
+                  sensorData["yaw"] ** 2,
                 ),
               ).toFixed(2)}
           </div>
@@ -135,30 +132,30 @@ function Sensor() {
         <div className="sensor-data-title">
           Magnético X
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["mag_x"]).toFixed(2)}
+            {sensorData && Number(sensorData["mag_x"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Magnético Y
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["mag_y"]).toFixed(2)}
+            {sensorData && Number(sensorData["mag_y"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Magnético Z
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["mag_z"]).toFixed(2)}
+            {sensorData && Number(sensorData["mag_z"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Módulo
           <div className="sensor-data-value">
-            {sensorData[0] &&
+            {sensorData &&
               Number(
                 Math.sqrt(
-                  sensorData[0]["mag_x"] ** 2 +
-                  sensorData[0]["mag_y"] ** 2 +
-                  sensorData[0]["mag_z"] ** 2,
+                  sensorData["mag_x"] ** 2 +
+                  sensorData["mag_y"] ** 2 +
+                  sensorData["mag_z"] ** 2,
                 ),
               ).toFixed(2)}
           </div>
@@ -170,7 +167,7 @@ function Sensor() {
         <div className="sensor-data-title">
           Graus
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["esterc"]).toFixed(2)}
+            {sensorData && Number(sensorData["esterc"]).toFixed(2)}
           </div>
         </div>
       </div>
@@ -180,7 +177,7 @@ function Sensor() {
         <div className="sensor-data-title">
           RPM
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["rot"]).toFixed(2)}
+            {sensorData && Number(sensorData["rot"]).toFixed(2)}
           </div>
         </div>
       </div>
@@ -190,7 +187,7 @@ function Sensor() {
         <div className="sensor-data-title">
           Temperatura Atual
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["temp"]).toFixed(2)}
+            {sensorData && Number(sensorData["temp"]).toFixed(2)}
           </div>
         </div>
       </div>
@@ -200,7 +197,7 @@ function Sensor() {
         <div className="sensor-data-title">
           Velocidade Linear Atual
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["veloc"]).toFixed(2)}
+            {sensorData && Number(sensorData["veloc"]).toFixed(2)}
           </div>
         </div>
       </div>
@@ -210,13 +207,13 @@ function Sensor() {
         <div className="sensor-data-title">
           Longitude:
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["long"]).toFixed(2)}
+            {sensorData && Number(sensorData["long"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Latitude:
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["lat"]).toFixed(2)}
+            {sensorData && Number(sensorData["lat"]).toFixed(2)}
           </div>
         </div>
       </div>
@@ -226,13 +223,13 @@ function Sensor() {
         <div className="sensor-data-title">
           Velocidade Roda:
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["press_ar"]).toFixed(2)}
+            {sensorData && Number(sensorData["press_ar"]).toFixed(2)}
           </div>
         </div>
         <div className="sensor-data-title">
           Altitude:
           <div className="sensor-data-value">
-            {sensorData[0] && Number(sensorData[0]["altitude"]).toFixed(2)}
+            {sensorData && Number(sensorData["altitude"]).toFixed(2)}
           </div>
         </div>
       </div>
--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -2,11 +2,18 @@ import "./App.css";
 import Sensor from "../components/Sensor";
 import Action from "../components/Action";
 
+import { io } from 'socket.io-client';
+
 function App() {
+  let url: string = import.meta.env.VITE_BACKEND_URL;
+  url = url.replace("http", "ws"); // Change this to HTTPS if not in localhost
+
+  let socket = io(url);
+
   return (
     <div className="app">
       <div className="sensor-div">
-        <Sensor />
+        <Sensor socket={socket} />
       </div>
       <div className="div-action">
         <Action />
