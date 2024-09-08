c41b14e3c298c629646ef8893dc315dc1cf2e815
Lucas Bernardino <lucasgber@gmail.com>
2024-09-08 14:42:08 -0300

    Add NavigationRail

---
+++ lavisualizar/lib/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.all,
+              destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.poll_outlined),
+                selectedIcon: Icon(Icons.poll),
+                label: Text("Gráficos")
+              ),
+              NavigationRailDestination(
+                  icon: Icon(Icons.settings_applications_outlined),
+                  selectedIcon: Icon(Icons.settings_applications, ),
+                  label: Text("Configurações")
+              )
+            ],
+          selectedIndex: _selectedIndex
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+        ],
+      ),
+    );
+  }
+}
