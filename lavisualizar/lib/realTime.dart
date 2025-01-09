d19bc1ea78830caa40f9e05555db659995e2b43b
Lucas Bernardino <lucasgber@gmail.com>
2025-01-09 09:48:30 -0300

    Add big feature where cartesian chart is rendered dynamically

--- lavisualizar/lib/realTime.dart
+++ lavisualizar/lib/realTime.dart
@@ -8,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+typedef MapChartController = Map<String, Map<String, dynamic>>;
+
 class RealTime extends StatefulWidget {
   const RealTime({super.key});
 
@@ -15,21 +17,24 @@ class RealTime extends StatefulWidget {
   State<RealTime> createState() => _RealTimeState();
 }
 
+
 class _RealTimeState extends State<RealTime> {
 
   late IO.Socket socket;
   late Timer? _timer;
   Map<String, dynamic> bikeInfo = initMap();
 
-  Map<String, List<dynamic>> listOfBikeInfo = initListOfBikeInfo();
-  Map<String, ChartSeriesController?> _chartSeriesController = {
-    "acel_x": null,
-    "acel_y": null,
-    "acel_z": null,
-    "vel_x": null,
-    "vel_y": null,
-    "vel_z": null,
-};
+
+  /*
+  The variable chartDataAndController shoud look like this:
+  {
+  "acel_x": {
+      "chartData": List<CartesianChartPoint>,
+      "controller": ChartSeriesController
+    }
+  }
+  */
+  MapChartController chartDataAndController = initMapChartController();
 
   @override
   void dispose() {
@@ -46,7 +51,7 @@ class _RealTimeState extends State<RealTime> {
   }
 
   initSocket() {
-    String api_url_socket = "wss://doc4981u1tzc.share.zrok.io"; // IF IT'S IN LOCALHOST, PLEASE CHANGE IT TO 'http' INSTEAD OF 'https'
+    String api_url_socket = "wss://js1ehn5fl00j.share.zrok.io"; // IF IT'S IN LOCALHOST, PLEASE CHANGE IT TO 'http' INSTEAD OF 'https'
     socket = IO.io(api_url_socket, <String, dynamic>{
       'autoConnect': false,
       'transports': ['websocket'],
@@ -60,13 +65,11 @@ class _RealTimeState extends State<RealTime> {
     socket.onError((err) => print(err));
     socket.on('send',(data){
       bikeInfo = jsonDecode(data);
-      updateBikeInfoList(bikeInfo, listOfBikeInfo, _chartSeriesController);
+      updateBikeInfoList(bikeInfo, chartDataAndController);
       setState(() {
         bikeInfo = jsonDecode(data);
-        listOfBikeInfo = listOfBikeInfo;
-        _chartSeriesController = _chartSeriesController;
+        chartDataAndController = chartDataAndController;
       });
-      log("listOfBikeInfo: $listOfBikeInfo");
     });
   }
 
@@ -81,13 +84,13 @@ class _RealTimeState extends State<RealTime> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
-                width: 450,
-                height: 300,
+                width: 700,
+                height: 700,
                 child: buildXYZCard("ACELERAÇÃO",
                     {"title": "Aceleração X", "value": "${bikeInfo["acel_x"]} m/s²"},
                     {"title": "Aceleração Y", "value": "${bikeInfo["acel_y"]} m/s²"},
                     {"title": "Aceleração Z", "value": "${bikeInfo["acel_z"]} m/s²"},
-                    _chartSeriesController["acel_x"]!, listOfBikeInfo)
+                    chartDataAndController)
               ),
               /*
               Container(
@@ -144,7 +147,7 @@ class _RealTimeState extends State<RealTime> {
   }
 }
 
-Widget buildXYZCard(String cardTitle, Map<String, String> dataX, Map<String, String> dataY, Map<String, String> dataZ, ChartSeriesController _controller, Map<String, List<dynamic>> _listOfBikeInfo) {
+Widget buildXYZCard(String cardTitle, Map<String, String> dataX, Map<String, String> dataY, Map<String, String> dataZ, MapChartController _chartController) {
   return Card(
     child: Padding(
       padding: const EdgeInsets.all(8.0),
@@ -178,74 +181,70 @@ Widget buildXYZCard(String cardTitle, Map<String, String> dataX, Map<String, Str
               ),
             ],
           ),
-          buildXYZChart(cardTitle, "acel_x", _controller, _listOfBikeInfo)
+          buildXYZChart(cardTitle, "acel_x", _chartController)
         ],
       ),
     ),
   );
 }
 
-Widget buildXYZChart(String title, String mapVal, ChartSeriesController _controller, Map<String, List<dynamic>> _listOfBikeInfo) {
+Widget buildXYZChart(String title, String mapVal, MapChartController _chartController) {
 
-  List<CartesianChartPoint> chartData = [];
-
-  int forLoopSize = _listOfBikeInfo["time"]?.length ?? 0;
-
-  for (int i = 0; i < forLoopSize; i++) {
-    chartData.add(CartesianChartPoint(_listOfBikeInfo["time"]?[i], _listOfBikeInfo[mapVal]?[i]));
-  }
-
-  return SfCartesianChart(
-      title: ChartTitle(
-        text: "${title} em função do tempo",
-        textStyle: TextStyle(
-          color: Colors.white,
-          fontSize: 14,
+  return Container(
+    width: 400,
+    height: 400,
+    child: SfCartesianChart(
+        title: ChartTitle(
+          text: "${title} em função do tempo",
+          textStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
         ),
-      ),
-      enableAxisAnimation: true,
-      tooltipBehavior: TooltipBehavior(
-        color: Colors.deepOrange,
-        enable: true,
-        borderColor: Colors.deepOrange,
-        borderWidth: 2,
-        header: "",
-      ),
-      zoomPanBehavior: ZoomPanBehavior(
-        enablePanning: true,
-        enableMouseWheelZooming: true,
-        enablePinching: true,
-      ),
-      primaryXAxis: NumericAxis(
-          labelStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.w500),
-          title: AxisTitle(text: "Pontos")
-      ),
-      primaryYAxis: NumericAxis(
-        labelStyle: const TextStyle(
-          color: Colors.white,
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
+        enableAxisAnimation: true,
+        tooltipBehavior: TooltipBehavior(
+          color: Colors.deepOrange,
+          enable: true,
+          borderColor: Colors.deepOrange,
+          borderWidth: 2,
+          header: "",
         ),
-        title: AxisTitle(
-            text: "TITULO"),
-      ),
-      series: <CartesianSeries>[
-        LineSeries<CartesianChartPoint, DateTime>(
-            onRendererCreated: (ChartSeriesController controller) {
-               _controller = controller;
-            },
-            color: Colors.lightBlue.shade900,
-            width: 3.5,
-            dataSource: chartData,
-            xValueMapper: (CartesianChartPoint point, _) => point.date,
-            yValueMapper: (CartesianChartPoint point, _) => point.value,
-            enableTooltip: true,
-            dataLabelSettings:DataLabelSettings(isVisible : true, color: Colors.lightBlue.shade700, borderRadius: 20)
-        )
-      ]
+        zoomPanBehavior: ZoomPanBehavior(
+          enablePanning: true,
+          enableMouseWheelZooming: true,
+          enablePinching: true,
+        ),
+        primaryXAxis: DateTimeAxis(
+            labelStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w500),
+            title: AxisTitle(text: "Pontos")
+        ),
+        primaryYAxis: NumericAxis(
+          labelStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          title: AxisTitle(
+              text: "TITULO"),
+        ),
+        series: <CartesianSeries>[
+          LineSeries<CartesianChartPoint, DateTime>(
+              onRendererCreated: (ChartSeriesController controller) {
+                _chartController[mapVal]?["controller"] = controller;
+              },
+              color: Colors.lightBlue.shade900,
+              width: 3.5,
+              dataSource: _chartController[mapVal]?["chartData"],
+              xValueMapper: (CartesianChartPoint point, _) => point.date,
+              yValueMapper: (CartesianChartPoint point, _) => point.value,
+              enableTooltip: true,
+              dataLabelSettings:DataLabelSettings(isVisible : true, color: Colors.lightBlue.shade700, borderRadius: 20)
+          )
+        ]
+    ),
   );
 }
 
@@ -255,36 +254,27 @@ atualizar a lista dos valores
 setState no decode e na lista
 */
 
-void updateBikeInfoList(Map<String, dynamic> _bikeInfo, Map<String, List<dynamic>> _listOfBikeInfo, Map<String, ChartSeriesController?> _controller) async {
+void updateBikeInfoList(Map<String, dynamic> _bikeInfo, MapChartController _chartController) async {
 
   DateTime currentTime = DateTime.now();
 
-  _listOfBikeInfo["acel_x"]?.add(_bikeInfo["acel_x"]);
-  _listOfBikeInfo["acel_y"]?.add(_bikeInfo["acel_y"]);
-  _listOfBikeInfo["acel_x"]?.add(_bikeInfo["acel_z"]);
+  _chartController.forEach((key, subMap) {
 
-  _listOfBikeInfo["vel_x"]?.add(_bikeInfo["vel_x"]);
-  _listOfBikeInfo["vel_y"]?.add(_bikeInfo["vel_y"]);
-  _listOfBikeInfo["vel_z"]?.add(_bikeInfo["vel_z"]);
+    subMap["chartData"].add(CartesianChartPoint(currentTime, _bikeInfo[key] as num));
 
-  _listOfBikeInfo["time"]?.add(currentTime);
-
-  _listOfBikeInfo.forEach((key, listOfPoints) {
-    if (listOfPoints.length > 100) {
-      listOfPoints.removeAt(0);
+    if (subMap["chartData"].length == 10) {
+      subMap["chartData"].removeAt(0);
+      subMap["controller"]?.updateDataSource(
+        addedDataIndexes: <int>[subMap["chartData"].length - 1],
+        removedDataIndexes: <int>[0],
+      );
+    } else {
+      subMap["controller"]?.updateDataSource(
+        addedDataIndexes: <int>[subMap["chartData"].length - 1],
+      );
     }
-    _controller[key]?.updateDataSource(addedDataIndexes: <int>[listOfPoints.length - 1], removedDataIndexes: <int>[0]);
   });
 
-  if (_controller != null) {
-    print("N SOU NULO 1");
-  }
-  if (_controller["acel_x"] != null ) {
-    print("N SOU NULO 2");
-  }
-
-  log("_listOfBikeInfo: $_listOfBikeInfo");
-  log("controller: $_controller");
 }
 
 Map<String, dynamic> initMap() {
@@ -307,26 +297,64 @@ Map<String, dynamic> initMap() {
   return newMap;
 }
 
-Map<String, List<dynamic>> initListOfBikeInfo() {
-  Map<String, List<dynamic>> newList = {
-    "id": [],
-    "acel_x": [],
-    "acel_y": [],
-    "acel_z": [],
-    "vel_x": [],
-    "vel_y": [],
-    "vel_z": [],
-    "roll": [],
-    "pitch": [],
-    "yaw": [],
-    "esterc": [],
-    "long": [],
-    "lat": [],
-    "veloc": [],
+MapChartController initMapChartController() {
+  MapChartController newMap = {
+    "acel_x": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "acel_y": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "acel_z": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "vel_x": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "vel_y": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "vel_z": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "roll": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "pitch": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "yaw": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "esterc": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "long": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "lat": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
+    "veloc": {
+      "controller": null,
+      "chartData": List<CartesianChartPoint>.empty(growable: true)
+    },
   };
-
-  return newList;
+  return newMap;
 }
+
 
 class CartesianChartPoint {
   CartesianChartPoint(this.date, this.value);
